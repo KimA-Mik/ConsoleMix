d041797f1cd4279c2387b35347011a48254217f2
KimA-Mik <ananim38@gmail.com>
2022-09-27 18:18:18 +0300

    Added new project

---
+++ BenchSpeedTest/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<FibonacciBench>();
+
+[MemoryDiagnoser]
+public class FibonacciBench
+{
+    [Params(25)]
+    public long index;
+
+    [Benchmark]
+    public void BenchDynamicMethod()
+    {
+        var res = DynamicWay(index);
+    }
+    
+    [Benchmark]
+    public void BenchRecursiveMethod()
+    {
+        var res = RecursiveWay(index);
+    }
+    
+    public long DynamicWay(long ind)
+    { 
+        long[] arr = new long[ind];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (long i = 2; i < arr.Length; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+        
+        return arr[ind-1];
+    }
+
+    public long RecursiveWay(long ind)
+    {
+        if (ind < 3)
+            return 1;
+
+        return RecursiveWay(ind - 1) + RecursiveWay(ind - 2);
+    }
+}
